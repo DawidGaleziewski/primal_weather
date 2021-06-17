0e6a3102a55012694bac296cc0f811a0944e783c
Gałęziewski Dawid <Dawid.Galeziewski@external.t-mobile.pl>
2021-06-17 22:06:42 +0200

    update: started adding react vis line chart

---
+++ src/components/LineChart/style/lineChartStyle.ts
@@ -0,0 +1,10 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
+import { css } from '@emotion/react';
+
+const lineChartStyle =  css`
+    height: 500px;
+`;
+
+export default lineChartStyle;
--- src/views/WeatherView/WeatherView.tsx
+++ src/views/WeatherView/WeatherView.tsx
@@ -1,19 +1,62 @@
 import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+// Components
+import LineChart from '@Components/LineChart/LineChart';
+
+// MUI
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 // Redux actions
 import {fetchData} from '@Redux/features/selectedRegionData/selectedRegionData';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
+
 const WeatherView = () => {
+    const classes = useStyles();
     const dispatch = useDispatch();
 
     useEffect(()=> {
         dispatch(fetchData({query: 'test', days: 2}))
     }, [])
 
-    return <div>
-        Weather view
-    </div>
+    return <main className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+              <LineChart />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>xs=6 sm=3</Paper>
+        </Grid>
+      </Grid>
+    </main>
 }
 
 export default WeatherView;
